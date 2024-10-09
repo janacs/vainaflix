2b5e63d7aab0e1d9330fad8ce71e7fe688f4ba84
Janderson <jandersonsouza_phn@hotmail.com>
2024-10-09 10:33:46 -0300

    primeiroCommit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import "./global.scss"
+
+function App(){
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                    <li>Saiba Mais</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
